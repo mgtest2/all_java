e271f085516480f32c66ab8e938e2597320f9cda
mgtest2 <mgtest2@test.com>
2017-01-23 21:50:15 +0000

    ddds

---
+++ src/com/my/tdd/main/StaticService.java
@@ -0,0 +1,9 @@
+package com.my.tdd.main;
+
+public class StaticService {
+
+	public static String test(){
+		return "test";
+	}
+	
+}
---
+++ src/com/my/tdd/main/StaticServiceConsumer.java
@@ -0,0 +1,12 @@
+package com.my.tdd.main;
+
+public class StaticServiceConsumer {
+
+	
+	public String test(){
+		
+		return StaticService.test();
+		
+	}
+	
+}
